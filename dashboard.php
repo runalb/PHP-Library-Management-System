39b21363af0e87f223a19389e2925ed0111f4560
Runal <runalb@users.noreply.github.com>
2021-05-29 01:00:16 +0530

    FIxed - indentation

--- dashboard.php
+++ dashboard.php
@@ -15,57 +15,66 @@ session_start();
     <link rel="stylesheet" href="vender/css/all.css">
 </head>
 <body>
-    <?php
-        if($_SESSION["userid"] === ""){
-          echo $_SESSION['userid'];
-          echo "login";
-          header("Location: index.php ");
-        }
-      ?>
-    <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-        <a class="navbar-brand my-0 mr-md-auto" href="dashboard.php">
-            <img src="images/simplelogo-dark.svg" alt="logo" width="130" height="30" alt="Logo" loading="lazy">
-        </a>
+  <?php
+      if($_SESSION["userid"] === ""){
+        echo $_SESSION['userid'];
+        echo "login";
+        header("Location: index.php ");
+      }
+  ?>
+
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <a class="navbar-brand my-0 mr-md-auto" href="dashboard.php">
+        <img src="images/simplelogo-dark.svg" alt="logo" width="130" height="30" alt="Logo" loading="lazy">
+    </a>
         
-        <button class="btn btn-outline logout-btn" onclick="location.href='user-logout.php'">Logout</button>
+    <button class="btn btn-outline logout-btn" onclick="location.href='user-logout.php'">Logout</button>
+  </div>
+
+  <div class="container">
+    <div class="row my-4">
+
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='add-books.php'">
+          <i class="far fa-plus-square pr-3"></i> Add Books
+        </button>
       </div>
 
-      <div class="container">
-          <div class="row my-4">
-              <div class="col-sm">
-                <button class="btn btn-block action-btn p-3" onclick="location.href='add-books.php'">
-                    <i class="far fa-plus-square pr-3"></i> Add Books
-                </button>
-              </div>
-              <div class="col-sm">
-                <button class="btn btn-block action-btn p-3" onclick="location.href='search-books.php'">
-                    <i class="fas fa-search pr-3"></i> Search
-                </button>
-            </div>
-            <div class="col-sm">
-                <button class="btn btn-block action-btn p-3" onclick="location.href='view-books.php'">
-                    <i class="fas fa-stream pr-3"></i> View Books
-                </button>
-            </div>
-          </div>
-          <div class="row my-4">
-            <div class="col-sm">
-              <button class="btn btn-block action-btn p-3" onclick="location.href='issue-book.php'">
-                <i class="fas fa-book pr-3"></i> Issue Books
-              </button>
-            </div>
-            <div class="col-sm">
-              <button class="btn btn-block action-btn p-3" onclick="location.href='return-books.php'">
-                <i class="fas fa-book pr-3"></i> Return Books
-              </button>
-          </div>
-          <div class="col-sm">
-              <button class="btn btn-block action-btn p-3" onclick="location.href='add-admin.php'">
-                <i class="fas fa-user-shield pr-3"></i> Admin
-              </button>
-          </div>
-        </div>
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='search-books.php'">
+          <i class="fas fa-search pr-3"></i> Search
+        </button>
       </div>
+
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='view-books.php'">
+          <i class="fas fa-stream pr-3"></i> View Books
+        </button>
+      </div>
+
+    </div>
+
+    <div class="row my-4">
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='issue-book.php'">
+          <i class="fas fa-book pr-3"></i> Issue Books
+        </button>
+      </div>
+
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='return-books.php'">
+          <i class="fas fa-book pr-3"></i> Return Books
+        </button>
+      </div>
+
+      <div class="col-sm">
+        <button class="btn btn-block action-btn p-3" onclick="location.href='add-admin.php'">
+          <i class="fas fa-user-shield pr-3"></i> Admin
+        </button>
+      </div>
+
+    </div>
+  </div>
       
 </body>
 </html>
