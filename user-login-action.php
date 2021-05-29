28fe9b526803e259c772264c4082b67100e23c90
Runal <runalb@users.noreply.github.com>
2021-05-29 12:53:49 +0530

    Rename - login page

--- user-login-action.php
+++ user-login-action.php
@@ -10,7 +10,7 @@ session_start();
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets/dist/css/bootstrap.min.css" >
     <link rel="stylesheet" href="style.css">
-    <title>Login</title>
+    <title>Login - Action</title>
 </head>
 <body>
 
